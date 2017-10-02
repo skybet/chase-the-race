8b33e20c39174ea8261a48ddf50e4e9d44aacee9
Adriano Gimenez <agi01@sbgml02465.sbg.skybettingandgaming.com>
2017-10-02 13:54:34 +0100

    initial email form

---
+++ emailForm.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Chase the Race</title>
+  </head>
+  <body>
+    <h1>Chase the Race</h1>
+    <form class="" action="" method="post" onsubmit="">
+      <p>Please enter your email address:</p>
+      <input type="text" name="email">
+      <input type="submit" name="submit" value="submit">
+    </form>
+    <script type="scripts/javascript"></script>
+  </body>
+</html>
