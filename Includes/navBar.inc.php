87ebcfe400ced48ea5ea34291a1868efb97f5df1
alicerose1812 <alice.palmer@skybettingandgaming.com>
2017-10-02 15:05:53 +0100

    Header branding

---
+++ Includes/navBar.inc.php
@@ -0,0 +1,7 @@
+<div id="navHeader">
+  <div class="container">
+    <div id="logoDiv">
+      <img src="/images/logo.png" alt="logo">
+    </div>
+  </div>
+</div>
--- views/emailForm.php
+++ views/emailForm.php
@@ -3,13 +3,17 @@
   <head>
     <meta charset="utf-8">
     <title>Chase the Race</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/chaseTheRaceStyle.css"/>
+    <link rel="icon" href="../images/favicon.ico" type="image/x-icon"/>
   </head>
   <body>
+    <?php
+            include_once('../includes/navBar.inc.php');
+    ?>
     <h1>Chase the Race</h1>
     <form class="emailForm" action="" method="post">
       <p>Please enter your email address:</p>
-      <input type="email" name="email">
+      <input type="text" name="email">
       <input type="submit" name="submit" value="submit">
     </form>
   </body>
