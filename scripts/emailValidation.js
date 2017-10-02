6d68b3fede9eae496b1c9ee7b5e88bc85c3e8cfc
Adriano Gimenez <agi01@sbgml02465.sbg.skybettingandgaming.com>
2017-10-02 14:23:03 +0100

    regex for email

--- scripts/emailValidation.js
+++ scripts/emailValidation.js
@@ -1,4 +1,25 @@
-function validateEmail() {
-  email = document.getElementsByName('email');
-  if 
+// function validateEmail() {
+//   email = document.getElementsByName('email');
+//   if
+// }
+
+function validateEmail(email) {
+  var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
 }
+
+function validate() {
+  $("#result").text("");
+  var email = $("[name=email]").val();
+  if (validateEmail(email)) {
+    $("#result").text(email + " is valid :)");
+    $("#result").css("color", "green");
+    return false
+  } else {
+    $("#result").text(email + " is not valid :(");
+    $("#result").css("color", "red");
+  }
+  return false;
+}
+
+$("#validate").bind("click", validate);
--- views/emailForm.php
+++ views/emailForm.php
@@ -3,14 +3,16 @@
   <head>
     <meta charset="utf-8">
     <title>Chase the Race</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   </head>
   <body>
     <h1>Chase the Race</h1>
-    <form class="" action="" method="post" onsubmit="">
+    <form class="emailForm" action="" method="post" onsubmit="return validate">
       <p>Please enter your email address:</p>
       <input type="text" name="email">
-      <input type="submit" name="submit" value="submit">
+      <button id='validate' type="submit" name="submit" value="submit">validate</button>
     </form>
+    <p id=result></p>
     <script type="scripts/javascript"></script>
   </body>
 </html>
