d5ecd511a6ca8aa78a8e057c50af2943c2766f07
preeny95 <preeny@sky.com>
2017-10-02 15:15:09 +0100

    new database schema

--- ChaseTheRace.sql
+++ ChaseTheRace.sql
@@ -1,26 +1,14 @@
-create database ChaseTheRace;
+DROP TABLE IF EXISTS User;
 
-create table User (User_id int not null auto_increment, User_email varchar(50) not null, Primary KEY(User_id));
-
-create table Team (Team_id int not null auto_increment, Team_name varchar(50) not null, Primary KEY (Team_id));
-
-create table Driver (Driver_id int not null auto_increment, Driver_name varchar(50) not null, Team_id int not null,
-  Primary KEY(Driver_id),
-  Foreign KEY Team_id REFERENCES Team(Team_id));
-
-create table Race (Race_id int not null auto_increment, Location varchar(50) not null, Primary KEY(Race_id));
-
-create table RaceDrivers (RaceDriver_id int not null auto_increment, Race_id int not null, Driver_id int not null,
-  Primary KEY(RaceDriver_id),
-  Foreign KEY Race_id REFERENCES Race(Race_id)),
-  Foreign KEY Driver_id REFERENCES Driver(Driver_id);
-
-create table Predictions (Predictions_id int not null auto_increment, User_id int not null, RaceDriver_id int not null, NoOfPitStops int not null,
-  Primary KEY(Predictions_id),
-  Foreign KEY User_id REFERENCES User(User_id),
-  Foreign KEY RaceDriver_id REFERENCES RaceDrivers(RaceDriver_id)
+create table Users (
+  id int not null auto_increment primary key, 
+  email varchar(250) not null
 );
 
-create table Results (Result_id int not null auto_increment, RaceDriver_id int not null,
-  Primary KEY(Result_id),
-  Foreign KEY RaceDriver_id REFERENCES RaceDrivers(RaceDriver_id));
+DROP TABLE IF EXISTS Predictions;
+
+create table Predictions (
+  id int not null auto_increment primary key, 
+  user_id int not null, 
+  prediction varchar(256) not null
+);
