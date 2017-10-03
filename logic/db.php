260fb5b8f63961bd0abe776c8ea45252f204fd7a
preeny95 <preeny@sky.com>
2017-10-03 13:43:32 +0100

    Databse connection fix

--- logic/db.php
+++ logic/db.php
@@ -4,7 +4,7 @@ function getDB(){
   if (isset($pdo)){
       return $pdo;
   }
-  $host = 'us-cdbr-iron-east-05.cleardb.net/heroku_42070fbe6866ca9?reconnect=true';
+  $host = 'us-cdbr-iron-east-05.cleardb.net';
   $db   = 'heroku_42070fbe6866ca9';
   $user = 'b1f075b4451a27';
   $pass = 'a837a4fc';
