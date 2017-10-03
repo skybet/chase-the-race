37e286000af014f7492d031b537d9cdea1446a0b
preeny95 <preeny@sky.com>
2017-10-03 12:07:50 +0100

    Change database to connect to heroku

--- logic/db.php
+++ logic/db.php
@@ -4,11 +4,10 @@ function getDB(){
   if (isset($pdo)){
       return $pdo;
   }
-
-  $host = '127.0.0.1';
-  $db   = 'ChaseTheRace';
-  $user = 'adminUser';
-  $pass = 'The password';
+  $host = 'us-cdbr-iron-east-05.cleardb.net/heroku_42070fbe6866ca9?reconnect=true';
+  $db   = 'heroku_42070fbe6866ca9';
+  $user = 'b1f075b4451a27';
+  $pass = 'a837a4fc';
 
   $dsn = "mysql:host=$host;dbname=$db";
   $pdo = new PDO($dsn, $user, $pass);
