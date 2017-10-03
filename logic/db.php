625d0df404057b1e7ae69c372af4bfe4584a0ecb
preeny95 <preeny@sky.com>
2017-10-03 15:48:53 +0100

    final database

--- logic/db.php
+++ logic/db.php
@@ -1,32 +1,18 @@
 <?php
-// function getDB(){
-//   static $pdo;
-//   if (isset($pdo)){
-//       return $pdo;      
-//   }
-$host = 'us-cdbr-iron-east-05.cleardb.net';
-$db   = 'heroku_2a00e25f9fd5fde';
-$user = 'b853c9a50b3dae';
-$pass = '850596d0';
+function getDB(){
+  static $pdo;
+  if (isset($pdo)){
+      return $pdo;      
+  }
+  
+  $host = 'us-cdbr-iron-east-05.cleardb.net';
+  $db   = 'heroku_2a00e25f9fd5fde';
+  $user = 'b853c9a50b3dae';
+  $pass = '850596d0';
 
-$mysqli = new mysqli($host, $user, $pass, $db);
+  $dsn = "mysql:host=$host;dbname=$db";
+  $pdo = new PDO($dsn, $user, $pass);
 
-/* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
+  return $pdo;
 }
-
-  if ($result = $mysqli->query("SELECT * FROM Users")) {
-    printf("Select returned %d rows.\n", $result->num_rows);
-
-    /* free result set */
-    $result->close();
-}
-
-  // $dsn = "mysql:host=$host;dbname=$db";
-  // $pdo = new PDO($dsn, $user, $pass);
-
-  // return $pdo;
-// }
 ?>
