7ffa94d28c4cadcb328645832f5625db74f6ebbd
alicerose1812 <alice.palmer@skybettingandgaming.com>
2017-10-03 14:13:48 +0100

    ts and cs

---
+++ Includes/footer.inc.php
@@ -0,0 +1,35 @@
+<footer>
+           <div id="footerBottom">
+               <div class="w-50p disp-in fl-left fs-11">
+                   © 2017 Hestview Ltd. All Rights Reserved.
+               </div>
+               <div class="w-50p disp-in fl-right txt-right fs-11">
+                   <span>Terms and Conditions</span>
+                   <p>Race winner-
+
+                   Bets will be settled on the official podium result immediately following the race.
+                   FIA classifications will constitute the official result.
+                   Subsequent amendments after the podium presentation will not count for betting purposes.
+
+                   Driver of Fastest Lap-
+
+                   This market shall be settled on the driver who sets the fastest single lap of the race.
+                   Drivers who set their fastest lap on their final lap following the leader winning the race will still be eligible to win this market with their fastest lap.
+
+                   Safety Car come on?
+
+                   Bets will be settled on whether or not the Safety Car is used in the race. The Safety Car will be deemed to have been used if Race Control signal for the car to join the track.
+                   Bets on the safety car will only be settled if a physical safety car enters the track at any point during the race. Virtual safety car will not count and bets will not be settled as “Yes” based solely upon this.
+                   Should the race start under Safety Car conditions, bets on this market will be made void.
+
+                   First Retiree-
+
+                   Bets will be settled on the first driver to retire a car from the race.
+                   Should more than one car retire on the same lap, dead-heat rules apply.
+                   Only cars who are lined up on the grid when the lights go out to signal the start of the race will be deemed to be starters, and eligable to be a "First Driver to Retire" - cars who fail to complete the warm-up lap, or whom do not reach the starting grid will be made void.
+
+                 </p>
+               </div>
+           </div>
+       </div>
+   </footer>
--- views/emailForm.php
+++ views/emailForm.php
@@ -13,6 +13,7 @@
 
     <?php
                 include_once(__DIR__.'/../includes/navBar.inc.php');
+                include_once(__DIR__.'/../includes/footer.inc.php');
     ?>
     <div class="container">
       <h1 class="pageTitle">Chase the Race</h1>
@@ -21,7 +22,7 @@
         <input type="email" name="email" required>
 
         <h2>Place Your Bets!</h2>
-      
+
           Choose a race winner!
           <select name="prediction" id="">
                   <option value="1">Lewis Hamilton </option>
@@ -52,6 +53,8 @@
           <br/><br/>
           <input class="submitButton" type="submit" name="submit" value="Enter">
         </form>
+        <br/>
+        <br/>
 
         <p id=result></p>
   </div>
