c15e97602e894d7ea7a47741ab7c8a1d435a1d79
Daniel Beckwith <dbe01@sbgml02473.sbg.skybettingandgaming.com>
2017-10-03 15:25:27 +0100

    email validation

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -1,10 +1,16 @@
 function checkNotNull(){
     var tieBreak;
     var email;
+    var validation = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     tieBreak = document.getElementById("tiebreaker").value;
     email = document.getElementById("email").value;
     if (!tieBreak || !email){
         alert("Please ensure all fields are complete!");
+        return false;
+    }
+    if (!validation.test(email.value)){
+        alert("Please provide a valid address!");
+        email.focus;
         return false;
     }
 }
