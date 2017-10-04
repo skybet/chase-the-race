da9d8cefde0264b53d4639a79047e8f9da5a3342
preeny95 <preeny@sky.com>
2017-10-04 15:28:46 +0100

    email

--- email.php
+++ email.php
@@ -4,7 +4,7 @@
 // If you are using Composer (recommended)
 // require 'vendor/autoload.php';
 // If you are not using Composer
-require(__DIR__."/vendor/sendgrid/sendgrid-php.php");
+require(__DIR__."/vendor/sendgrid/sendgrid/sendgrid-php.php");
 $from = new SendGrid\Email("Example User", "test@example.com");
 $subject = "Sending with SendGrid is Fun";
 $to = new SendGrid\Email("Example User", "nathanpreen@gmail.com");
