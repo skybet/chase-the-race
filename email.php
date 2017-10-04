3a07012d96bb79302d544a0290cbe9dc4ba47234
preeny95 <preeny@sky.com>
2017-10-04 15:18:50 +0100

    email

--- email.php
+++ email.php
@@ -1,24 +1,34 @@
 <?php
-
-
-
-
-
-// using SendGrid's PHP Library
-// https://github.com/sendgrid/sendgrid-php
-// If you are using Composer (recommended)
 require 'vendor/autoload.php';
-// If you are not using Composer
-// require("path/to/sendgrid-php/sendgrid-php.php");
-$from = new SendGrid\Email("Example User", "test@example.com");
-$subject = "Sending with SendGrid is Fun";
-$to = new SendGrid\Email("Example User", "nathanpreen@gmail.com");
-$content = new SendGrid\Content("text", "and easy to do anywhere, even with PHP");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-print_r($response->headers());
-echo $response->body();
+
+$sendgrid = new SendGrid("SENDGRID_APIKEY");
+$email    = new SendGrid\Email();
+
+$email->addTo("nathanpreen@gmail.com")
+      ->setFrom("you@youremail.com")
+      ->setSubject("Sending with SendGrid is Fun")
+      ->setHtml("and easy to do anywhere, even with PHP");
+
+$sendgrid->send($email);
+
+
+
+
+// // using SendGrid's PHP Library
+// // https://github.com/sendgrid/sendgrid-php
+// // If you are using Composer (recommended)
+// require 'vendor/autoload.php';
+// // If you are not using Composer
+// // require("path/to/sendgrid-php/sendgrid-php.php");
+// $from = new SendGrid\Email("Example User", "test@example.com");
+// $subject = "Sending with SendGrid is Fun";
+// $to = new SendGrid\Email("Example User", "nathanpreen@gmail.com");
+// $content = new SendGrid\Content("text", "and easy to do anywhere, even with PHP");
+// $mail = new SendGrid\Mail($from, $subject, $to, $content);
+// $apiKey = getenv('SENDGRID_API_KEY');
+// $sg = new \SendGrid($apiKey);
+// $response = $sg->client->mail()->send()->post($mail);
+// echo $response->statusCode();
+// print_r($response->headers());
+// echo $response->body();
 ?>
