1398b3e47bd009dc9377a5c45e6d92d98cdd8294
preeny95 <preeny@sky.com>
2017-10-04 11:58:00 +0100

    mailsend

--- createBet.php
+++ createBet.php
@@ -19,34 +19,55 @@ $predictionmanager = new PredictionManager(getDB());
 $predictionmanager->save($user->id, $_POST);
 
 //Composer's autoload file loads all necessary files
+// require 'vendor/autoload.php';
+
+// If you are using Composer
 require 'vendor/autoload.php';
 
-$mail = new PHPMailer;
+// If you are not using Composer (recommended)
+// require("path/to/sendgrid-php/sendgrid-php.php");
 
-$mail->isSMTP();  // Set mailer to use SMTP
-$mail->Host = 'smtp.mailgun.org';  // Specify mailgun SMTP servers
-$mail->SMTPAuth = true; // Enable SMTP authentication
-$mail->Username = 'postmaster@chase-the-race.mailgun.org'; // SMTP username from https://mailgun.com/cp/domains
-$mail->Password = 'a3daec95b1a90384c97acd336599f8b3'; // SMTP password from https://mailgun.com/cp/domains
-$mail->SMTPSecure = 'tls';   // Enable encryption, 'ssl'
+$from = new SendGrid\Email(null, "test@example.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "nathanpreen@gmail.com");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$mail->From = 'excited@chase-the-race.mailgun.org'; // The FROM field, the address sending the email
-$mail->FromName = 'Orlie'; // The NAME field which will be displayed on arrival by the email client
-$mail->addAddress($_POST['email'], 'BOB');     // Recipient's email address and optionally a name to identify him
-$mail->isHTML(true);   // Set email to be sent as HTML, if you are planning on sending plain text email just set it to false
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
 
-// The following is self explanatory
-$mail->Subject = 'Email sent with Mailgun';
-$mail->Body    = '<span style="color: red">Mailgun rocks</span>, thank you <em>phpmailer</em> for making emailing easy using this <b>tool!</b>';
-$mail->AltBody = 'Mailgun rocks, shame you can\'t see the html sent with phpmailer so you\'re seeing this instead';
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
 
-if(!$mail->send()) {
-    echo "Message hasn't been sent.";
-    echo 'Mailer Error: ' . $mail->ErrorInfo . "\n";
-} else {
-    echo "Message has been sent :) \n";
 
-}
+// $mail = new PHPMailer;
+
+// $mail->isSMTP();  // Set mailer to use SMTP
+// $mail->Host = 'smtp.mailgun.org';  // Specify mailgun SMTP servers
+// $mail->SMTPAuth = true; // Enable SMTP authentication
+// $mail->Username = 'postmaster@chase-the-race.mailgun.org'; // SMTP username from https://mailgun.com/cp/domains
+// $mail->Password = 'a3daec95b1a90384c97acd336599f8b3'; // SMTP password from https://mailgun.com/cp/domains
+// $mail->SMTPSecure = 'tls';   // Enable encryption, 'ssl'
+
+// $mail->From = 'excited@chase-the-race.mailgun.org'; // The FROM field, the address sending the email
+// $mail->FromName = 'Orlie'; // The NAME field which will be displayed on arrival by the email client
+// $mail->addAddress($_POST['email'], 'BOB');     // Recipient's email address and optionally a name to identify him
+// $mail->isHTML(true);   // Set email to be sent as HTML, if you are planning on sending plain text email just set it to false
+
+// // The following is self explanatory
+// $mail->Subject = 'Email sent with Mailgun';
+// $mail->Body    = '<span style="color: red">Mailgun rocks</span>, thank you <em>phpmailer</em> for making emailing easy using this <b>tool!</b>';
+// $mail->AltBody = 'Mailgun rocks, shame you can\'t see the html sent with phpmailer so you\'re seeing this instead';
+
+// if(!$mail->send()) {
+//     echo "Message hasn't been sent.";
+//     echo 'Mailer Error: ' . $mail->ErrorInfo . "\n";
+// } else {
+//     echo "Message has been sent :) \n";
+
+// }
 
 header('Location: confirmation.php');
 ?>
