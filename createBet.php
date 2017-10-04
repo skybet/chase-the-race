42687539b833d5d57f5e203f054722a8fa6ae4d3
alex2467 <awo01@sbgml03149.sbg.skybettingandgaming.com>
2017-10-04 12:09:48 +0100

    get submission ip address

--- createBet.php
+++ createBet.php
@@ -6,6 +6,28 @@ include 'classes/prediction.php';
 include 'logic/db.php';
 require("sendgrid-php/sendgrid-php.php");
 
+function get_client_ip_server() {
+  $ipaddress = '';
+  if ($_SERVER['HTTP_CLIENT_IP'])
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+  else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  else if($_SERVER['HTTP_X_FORWARDED'])
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+  else if($_SERVER['HTTP_FORWARDED_FOR'])
+      $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+  else if($_SERVER['HTTP_FORWARDED'])
+      $ipaddress = $_SERVER['HTTP_FORWARDED'];
+  else if($_SERVER['REMOTE_ADDR'])
+      $ipaddress = $_SERVER['REMOTE_ADDR'];
+  else
+      $ipaddress = 'UNKNOWN';
+
+  return $ipaddress;
+}
+
+$ipAddress = get_client_ip_server();
+
 //print_r($_POST); //test for POST
 
 //echo $_POST['email'];
