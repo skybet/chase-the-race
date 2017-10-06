268baf3285132294483dce12dd18198a5461501f
preeny95 <preeny@sky.com>
2017-10-06 11:24:46 +0100

    draw driver

--- checkBets.php
+++ checkBets.php
@@ -25,6 +25,7 @@ $res = $stmt->fetchAll();
 
 echo $res[0]['user_id'];
 echo $res[0]['prediction'];
+echo $res[0]['drivers.DriverName'];
 echo $res[0]['fastest_pit_stop'];
 echo $res[0]['first_retiree'];
 echo $res[0]['safety_car'];
