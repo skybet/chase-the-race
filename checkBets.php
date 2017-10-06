37426f57f961a185fb9428732b593b59ad4f2005
preeny95 <preeny@sky.com>
2017-10-06 10:29:18 +0100

    Email test

--- checkBets.php
+++ checkBets.php
@@ -2,23 +2,24 @@
 include 'logic/db.php';
 include 'logic/sessions.php'; 
 
-// function DrawDriver(){
-// $dm= new Driver(getDB());
+function DrawDriver(){
+$dm= new Driver(getDB());
 
-// $stmt = $this->db->prepare("select user_id, prediction, fastest_pit_stop, first_retiree, safety_car, tiebreaker
-// from predictions
-// inner join users on predictions.user_id = users.id
-// inner join drivers on predictions.prediction = drivers.id
-// where users.id = :userid");
+$stmt = $this->db->prepare("select user_id, prediction, fastest_pit_stop, first_retiree, safety_car, tiebreaker
+from predictions
+inner join users on predictions.user_id = users.id
+inner join drivers on predictions.prediction = drivers.id
+where users.id = :userid");
 
 $user = unserialize (serialize ($_SESSION['user']));
 
 $user->email;
 
-// $r = $stmt->execute([
-//     'userid'  => $uid
-// ]);
+$r = $stmt->execute([
+    'userid'  => $user->id
+]);
 
-var_dump($user);
-
+var_dump($stmt);
+}
+DrawDriver();
 ?>
