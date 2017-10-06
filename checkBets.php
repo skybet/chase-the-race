164bafada703ad29cc825f60853f603440fe0f32
preeny95 <preeny@sky.com>
2017-10-06 11:38:08 +0100

    draw driver

--- checkBets.php
+++ checkBets.php
@@ -8,10 +8,12 @@ function DrawDriver(){
     $db = getDB();
 $dm= new Driver($db);
 
-$stmt = $db->prepare("select user_id, drivers.DriverName, prediction, fastest_pit_stop, first_retiree, safety_car, tiebreaker
+$stmt = $db->prepare("select user_id, D1.DriverName, D2.DriverName, D3.DriverName, safety_car, tiebreaker
 from predictions
 inner join users on predictions.user_id = users.id
-inner join drivers on predictions.prediction = drivers.id
+inner join drivers AS D1 on predictions.prediction = D1.id
+inner join drivers AS D2 on predictions.fastest_pit_stop = D2.id
+inner join drivers AS D3 on predictions.first_retiree = D3.id
 where users.id = :userid");
 
 $user = unserialize (serialize ($_SESSION['user']));
@@ -22,9 +24,7 @@ $stmt->execute([
     'userid'  => $is
     ]);
 $res = $stmt->fetchAll();
-// var_dump($res);
-echo $res[0]['user_id'];
-echo $res[0]['prediction'];
+var_dump($res);
 echo $res[0]['DriverName'];
 echo $res[0]['fastest_pit_stop'];
 echo $res[0]['first_retiree'];
